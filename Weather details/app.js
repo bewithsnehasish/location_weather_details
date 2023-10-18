93ab5ad13102fee6e7a094b4faa1481a9d96d216
bewithsnehasish <mandalsneh97@gmail.com>
2023-10-18 11:08:11 +0530

    Updated all the files having the Project
    Added the project files in order to find and fetch the data for the particular location.................

---
+++ Weather details/app.js
@@ -0,0 +1,34 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+const cityName = document.getElementById("city-name");
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+const datetime = document.getElementById('datetime');
+
+async function getData(cityname) {
+  const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=d2f34f983bbb4e9d81f130129231308&q=${cityname}&aqi=yes`);
+  return await promise.json()
+}
+
+
+button.addEventListener("click", async() =>{
+  
+  // const value = input.value;
+  // const result = await getData(value);
+  // console.log(result);
+  // cityName.innerText = `${result.location.name},${result.location.region} - ${result.location.country}`;
+  // datetime.innerText = "DATE & TIME";
+  // cityTime.innerText = result.location.localtime ;
+  // cityTemp.innerText = result.current.temp_c + "°C";
+  try{
+    const value = input.value;
+    const result = await getData(value);
+    console.log(result);
+    cityName.innerText = `${result.location.name},${result.location.region} - ${result.location.country}`;
+    datetime.innerText = "DATE & TIME";
+    cityTime.innerText = result.location.localtime ;
+    cityTemp.innerText = result.current.temp_c + "°C";
+  }catch(error){
+    cityTime.innerText = "Please Enter Correct Location!!";
+  }
+});
